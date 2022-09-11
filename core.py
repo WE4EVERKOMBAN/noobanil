edba03ee4b1ba3413ce28d7cc60d11a60ec6b7cc
WE4EVERKOMBAN <108579654+WE4EVERKOMBAN@users.noreply.github.com>
2022-09-11 20:56:27 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,8 +1,8 @@
 #Bot Token From @BotFather
-bot_token = "Bot Token Here"
+bot_token = "5735199248:AAFwz99jVbqvaYOjwL4zDaRrv1XuhFRfbXk"
 
 #Admins Telegram ID
-admins = [1468386562,1914289455]
+admins = [895687774,1835479201]
 
 #MongoDb Url For Databse
-mongo_url = "Mongo Url"
+mongo_url = "mongodb+srv://Data1:Data1@cluster0.ekxpn.mongodb.net/?retryWrites=true&w=majority"
